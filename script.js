4844f82f62252e44874843cfec565ab1dc5391be
sammrguez <sammrguez@gmail.com>
2023-01-30 15:51:41 -0600

    Crear HTML y CSS de popup, crear perfil

---
+++ script.js
@@ -0,0 +1,8 @@
+let btnEditProfile = documument.querySelector(".edit-button");
+let btnCloseEditProfile = document.querySelector(".form__button_type_close");
+let popupEditProfile = document.querySelector(".popup_type_edit-profile");
+
+function openEditProfile() {
+  popupEditProfile.showModal();
+}
+btnEditProfile.addEventListener("click");
