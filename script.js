68ced3b1298372ee8355cfb04dedd7720dc0a4b9
sammrguez <sammrguez@gmail.com>
2023-02-02 14:03:39 -0600

    función completa de popup perfil

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//funciones para abrir y cerrrar popups//
 const btnEditProfile = document.querySelector(".edit-button");
 const popupEditProfile = document.querySelector(".popup_type_edit-profile");
 const btnCloseEditProfile = document.querySelector(
@@ -5,6 +6,7 @@ const btnCloseEditProfile = document.querySelector(
 );
 
 function openEditProfile() {
+  cleanInput();
   if (popupEditProfile.classList.contains("popup_opened")) {
     popupEditProfile.classList.remove("popup_opened");
   } else {
@@ -49,3 +51,51 @@ function closeAddNewPlace() {
 }
 
 btnCloseNewPlace.addEventListener("click", closeAddNewPlace);
+
+//funciones para asignar información del perfil//
+
+const profileInfo = document.querySelector(".profile__info");
+const btnCreateProfile = document.querySelector(".form__submit-button_edit");
+
+btnCreateProfile.addEventListener("click", createProfile);
+
+let userName = document.querySelector(".form__item_type_name");
+let userProfession = document.querySelector(".profile__user-profession");
+
+function createProfile() {
+  cleanProfile();
+
+  let userName = document.querySelector(".form__item_type_name");
+  let userProfession = document.querySelector(".form__item_type_about-me");
+
+  profileInfo.insertAdjacentHTML(
+    "beforeend",
+    `<h2 class="profile__user-name">${userName.value}</h2>
+  <h3 class="profile__user-profession">${userProfession.value}</h3>`
+  );
+
+  cleanInput();
+  closeEditProfile();
+}
+
+btnCreateProfile.addEventListener("click", createProfile);
+
+// Elimina los cursos del carrito en el DOM
+function cleanProfile() {
+  const element1 = document.querySelector(".profile__user-name");
+  const element2 = document.querySelector(".profile__user-profession");
+  if (element1 !== null && element2 !== null) {
+    element1.remove();
+    element2.remove();
+    console.log("function working");
+  }
+
+  console.log(element1);
+  console.log(element2);
+  console.log("nulls");
+}
+
+function cleanInput() {
+  document.querySelector(".form__item_type_name").value = "";
+  document.querySelector(".form__item_type_about-me").value = "";
+}
