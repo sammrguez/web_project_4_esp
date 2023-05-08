6fa2f6ef1c5fde4f299781f080d8d81b07f72072
sammrguez <sammrguez@gmail.com>
2023-05-08 16:34:05 -0600

    initial cards working

---
+++ script/Card.js
@@ -0,0 +1,58 @@
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+const cardsContainer = document.querySelector(".card-container");
+
+class Card {
+  constructor(data, templateSelector) {
+    this._name = data.name;
+    this._link = data.link;
+    this._templateSelector = templateSelector;
+  }
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._templateSelector)
+      .content.querySelector(".place-card")
+      .cloneNode(true);
+
+    return cardElement;
+  }
+  generateCard() {
+    this._cardElement = this._getTemplate();
+    this._cardElement.querySelector(".place-card__name").textContent =
+      this._name;
+    this._cardElement.querySelector(".place-card__photo").src = this._link;
+
+    return this._cardElement;
+  }
+}
+
+initialCards.forEach((item) => {
+  const card = new Card(item, "#card-template");
+  const newCard = card.generateCard();
+
+  cardsContainer.append(newCard);
+});
