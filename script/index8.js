29541a9c371ae2dade82a84972366971729c9ef4
sammrguez <sammrguez@gmail.com>
2023-05-30 15:25:45 -0600

    popup father class Done

--- script/index8.js
+++ script/index8.js
@@ -1,6 +1,14 @@
 import { Card } from "./Card.js";
-import { initialCards, cardsContainer } from "./Data.js";
+import {
+  initialCards,
+  cardsContainer,
+  popupEditProfile,
+  btnEditProfile,
+  popupAddNewPlace,
+  btnAddNewPlace,
+} from "./Data.js";
 import Section from "./Section.js";
+import Popup from "./Popup.js";
 
 const defaultCardList = new Section(
   {
@@ -12,5 +20,13 @@ const defaultCardList = new Section(
       defaultCardList.addItem(newCard);
     },
   },
-  cardsContainer
+  ".card-container" //cardsContainer
 );
+
+// popup
+
+const popupProfile = new Popup(".popup_type_edit-profile"); //popupEditProfile
+const popupProfileListeners = popupProfile.setEventListeners(btnEditProfile);
+
+const popupPlace = new Popup(".popup_type_new-place"); //popupAddNewPlace
+const popupPlaceListeners = popupPlace.setEventListeners(btnAddNewPlace);
